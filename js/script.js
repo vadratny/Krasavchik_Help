0f75908e5b247a95575e77714bc57995a890a864
Vadratny <122795977+vadratny@users.noreply.github.com>
2025-09-06 18:18:01 +0400

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ async function send() {
     comment = document.getElementById("comment").value
     // 7796270109:AAEAKjkRxzeJ3gS7jsEX4wMlkAa3fOGBxnY
     url = "https://api.telegram.org/bot7796270109:AAEAKjkRxzeJ3gS7jsEX4wMlkAa3fOGBxnY/sendMessage"
-    text = "*USER*: _${fio}_\n\n*Lesson*: _${lesson}_\n\n*Comment*: _${comment}_"
+    text = "*USER*: "+fio+"\n\n*Lesson*: "+lesson+"\n\n*Comment*: "+comment
     const response = await fetch(url, {
         method: "POST",
         headers: { "Accept": "application/json", "Content-Type": "application/json" },
@@ -14,4 +14,5 @@ async function send() {
             'parse_mode':"MarkdownV2"
         })
     });
+
 }
